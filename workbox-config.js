8a17b3d97502f734cb9ecb0eef23de62feefc302
leonardovallem <leo.mvm@hotmail.com>
2020-05-09 17:23:29 -0300

    Workbox

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "globDirectory": ".",
   "globPatterns": [
-    "**/*.{html,css,png,jpg,jpeg,backup,php,json,yml,md,js,scss,ico,lock,less,txt,svg,eot,ttf,woff,woff2}"
+    "**/*.{html,css,png,jpg,jpeg,backup,php,yml,md,js,scss,ico,lock,less,txt,svg,eot,ttf,woff,woff2}"
   ],
   "swDest": "sw.js"
 };
