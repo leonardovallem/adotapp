5c3edf2526c8c652153d86555b9719dd0d00811a
leonardovallem <leo.mvm@hotmail.com>
2020-05-09 17:13:10 -0300

    Correções no service worker, 2 - Leo

---
+++ workbox-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "globDirectory": ".",
+  "globPatterns": [
+    "**/*.{html,css,png,jpg,jpeg,backup,php,json,yml,md,js,scss,ico,lock,less,txt,svg,eot,ttf,woff,woff2}"
+  ],
+  "swDest": "sw.js"
+};
