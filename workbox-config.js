8853a9265548608382516638d9d808b9829e095d
leonardovallem <leo.mvm@hotmail.com>
2020-05-09 18:53:37 -0300

    Correção final do service worker

--- workbox-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  "globDirectory": ".",
-  "globPatterns": [
-    "**/*.{html,css,png,jpg,jpeg,backup,php,yml,md,js,scss,ico,lock,less,txt,svg,eot,ttf,woff,woff2}"
-  ],
-  "swDest": "sw.js"
-};
