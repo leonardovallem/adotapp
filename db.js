2c8df8215a4bcf6885b6f172e4539723bddbe419
leonardovallem <leo.mvm@hotmail.com>
2020-05-12 14:24:16 -0300

    Alterações na database

--- db.js
+++ db.js
@@ -1,5 +1,51 @@
-function adicionaUsuario() {
+function adicionaUsuario(e) {
     var usuarios = [];
+    usuarios = JSON.parse(localStorage.getItem("usuarios") || "[]");
+
+    let emailUsuario = document.getElementById("inputEmail").value;
+
+    if(usuarios.length == 0){
+        const usuario = {
+            nome: this.inputNome.value,
+            sobrenome: this.inputSobrenome.value,
+            email: this.inputEmailCadastro.value,
+            senha: this.inputSenhaCadastro.value
+        }
+
+        usuarios = JSON.parse(localStorage.getItem("usuarios") || "[]");
+
+        usuarios.push(usuario);
+        localStorage.setItem("usuarios", JSON.stringify(usuarios));
+
+    }else{
+
+        for(i=0 ; i < usuarios.length ; i++){
+            if(usuarios[0].email.indexOf(emailUsuario) == -1){
+                const usuario = {
+                    nome: this.inputNome.value,
+                    sobrenome: this.inputSobrenome.value,
+                    email: this.inputEmailCadastro.value,
+                    senha: this.inputSenhaCadastro.value
+                }
+        
+                usuarios = JSON.parse(localStorage.getItem("usuarios") || "[]");
+        
+                usuarios.push(usuario);
+                localStorage.setItem("usuarios", JSON.stringify(usuarios));
+            }
+            else{
+                alert("Esse email já foi cadastrado.");
+                return false;
+            }
+        }
+    }        
+
+}
+
+function teste() {
+    var usuarios = [];
+    usuarios = JSON.parse(localStorage.getItem("usuarios") || "[]");
+
     const usuario = {
         nome: this.inputNome.value,
         sobrenome: this.inputSobrenome.value,
@@ -11,6 +57,25 @@ function adicionaUsuario() {
 
     usuarios.push(usuario);
     localStorage.setItem("usuarios", JSON.stringify(usuarios));
+}
 
-    console.log("Funcionou?");
+
+function mostraUsuario() {
+    var arrayUsuarios = JSON.parse(localStorage.getItem("usuarios") || "[]");
+    var nomeUsuario = arrayUsuarios[arrayUsuarios.length - 1].nome;
+    var sobrenomeUsuario = arrayUsuarios[arrayUsuarios.length - 1].sobrenome;
+    document.getElementById("nomeUsuario").innerHTML = `${nomeUsuario} ${sobrenomeUsuario}`; 
+}
+
+function checaUsuario() {
+    var arrayUsuarios = JSON.parse(localStorage.getItem("usuarios") || "[]");
+
+}
+
+function x() {
+    for(i=0 ; i < JSON.parse(localStorage.getItem("usuarios") || "[]").length ; i++){
+        console.log(JSON.parse(localStorage.getItem("usuarios") || "[]")[i].email);
+    }
+
+    
 }
